a63e6129c64b5870d592c2f7ed94ce17d5315a95
Jamerson Lima <jamerson.lima@mv.com.br>
2018-12-18 19:16:18 -0300

    Coleta de dados sobre o diretor e avalidção dos filmes por diretor.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -16,7 +16,7 @@ def get_tag_content(tag_name, text):
 
 def get_tag(tag_name, text):
     return [n.group(1) for n in 
-        re.finditer('%s(.+?)%s' % (('<%s>' % tag_name), 
+        re.finditer('%s(.+?)%s' % (('<%s(.*?)>' % tag_name), 
                                    ('</%s>' % tag_name)),
                     text)]
 
@@ -29,9 +29,9 @@ def get_tag_property(property_name, tag_name, text):
 				return match.group(1)
 	return None
 
-def get_request(url)
+def get_request(url):
 	r = requests.get(url)
-    return r.text
+	return r.text
 
 def collect_movie_files():
     dataset_page_response_text = get_request("https://datasets.imdbws.com/")
@@ -67,13 +67,49 @@ def collect_movie_files():
 					name_infos = line.split('\t')
 					names[name_infos[0]] = name_infos[1:]
 				json.dump(names, names_json)
-		elif ''
+		elif '':
+			pass
 		content_file.close()
 
 def get_director_info(director_id):
 	director_page_url = "https://www.imdb.com/name/%s/" % director_id
 	director_page_raw_html = get_request(director_page_url)
-	for a in get_tag_content
+	for a in get_tag('a', director_page_raw_html):
+		if 'birth_place' in a:
+			if 'USA' in get_tag_content('a', a): # adjust to get content only for one tag content
+				pass # set is american
+				break
+
+def get_favorite_directors():
+	top_directors_page_raw_html = get_request('https://www.imdb.com/list/ls008344500/')
+	director_names = []
+	for div in get_tag('div', top_directors_page_raw_html):
+		if 'list-item-content' in get_tag_property('class', 'div', div):
+			for a in get_tag('a', div):
+				if 'name' in get_tag_property('href', 'a', a):
+					director_names.append(get_tag_content('a', a))
+	return director_names
+
+def get_average_movie_ratings_per_director(director_name):
+	director_page_html = get_request('https://www.imdb.com/name/%s/' % director_name)
+	movie_ratings = []
+	for div in get_tag('div', director_page_html):
+		if 'filmo-category-section' in get_tag_property('class', 'div', div):
+			for a in get_tag('a', div):
+				movie_url = 'https://www.imdb.com/%s/' % get_tag_property('href', 'a', a)
+				movie_page_html = get_request(movie_url)
+				for span in get_tag('span', movie_page_html):
+					if 'ratingValue' in get_tag_property('itemprop', 'span', span):
+						movie_ratings.append(get_tag_content('span', span)) # modify to avoid empty evaluation films
+	return movie_ratings
+						
+
+
+def get_director_movie_ratings():
+	names = json.load("names.json")
+	for name in names.keys():
+		if 'director' in names[name][3]:
+			get_average_movie_ratings_per_director(name)
 
 if __name__ == '__main__':
     collect_movie_files()
--- moviedata/statistics/statistics.py
+++ moviedata/statistics/statistics.py
@@ -4,8 +4,8 @@ def average(int_set):
     return sum(int_set)/len(int_set)
 
 def standard_deviation(int_set):
-    average = average(int_set)
-    return sqrt(sum([(i - average)**2 for i in int_set])/len(int_set))
+    int_set_average = average(int_set)
+    return sqrt(sum([(i - int_set_average)**2 for i in int_set])/len(int_set))
 
 def conditional_probability(p_a, p_b):
     return (p_a*p_b)/p_b
